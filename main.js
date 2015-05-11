4060cbda96615cdbdcd5166d7dc9b7779149993b
Meade Slocomb <meade.slocomb@gmail.com>
2015-05-11 13:40:50 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,42 @@
+var givenName;
+// // Q: What does `givenName` equal right now?
+// It is undefined.
+
+givenName = "Tim";
+// // Q: What is `givenName` set to right now?
+// The string "Tim"
+
+givenName = "JD";
+// // Q: What is `givenName` set to right now?
+// The string "JD"
+
+var greeting = "Hello, how are you?";
+// // Q: What is `greeting` set to?
+// The string "Hello, how are you?"
+
+var high = 50;
+var low  = 10;
+
+
+var math = high - low;
+// // Q: What is `math` set to?
+// math is set to 40
+
+math = high - "5";
+// // Q: What is `math` set to?
+// math is set to 45
+
+// Create a variable to calculate Tim's age
+// The answer should read "Tim is 31 years old"
+var born = 1983;
+var today = 2015;
+
+var timAge = today - born
+
+// Store some information following in variables.
+var yourName = 'Meade';
+var instructorName = 'Tim';
+
+var statement = yourName + " is taking a class at The Iron Yard, my instructor's name is " + instructorName;
+
+console.log(statement);
